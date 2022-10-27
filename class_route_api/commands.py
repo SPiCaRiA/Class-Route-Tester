0996395dbaf63c39802b646a6be0ed94bc75af96
Weixuan Lin <wlin4@scu.edu>
2022-10-27 11:31:29 -0700

    Add CLI tools for adding dummy data and printing the database records

--- class_route_api/commands.py
+++ class_route_api/commands.py
@@ -4,11 +4,37 @@ Command line utility functions.
 :Authors: Weixuan Lin
 '''
 
+import time
+
 import click
 import uvicorn
 
 from .internal.config import settings
+from .internal.db import connect_db, db_exist
 from .internal.db import init_db as _init_db
+
+
+def create_db():
+    '''
+    Wrap the database creation and initialization with click prompts.
+    '''
+    click.echo('Creating and initializing the database...')
+    _init_db()
+    click.echo('Done.')
+
+
+def create_db_if_not_exist():
+    '''
+    Check if the database file exists. If not, prompt a message to see if it 
+    needs to be created.
+    '''
+    if not db_exist():
+        click.confirm(
+            '[WARNING] The database file is not found, do you want to create ' +
+            'it now?',
+            abort=True
+        )
+        create_db()
 
 
 @click.group(invoke_without_command=True)
@@ -38,12 +64,72 @@ def init_db() -> None:
     '''
     Clear the existing data and create new tables.
     '''
-    click.echo('Initializing the database.')
-    _init_db()
+    if db_exist():
+        click.confirm(
+            '[WARNING] The database file already exists, do you want to drop ' +
+            'all data and recreate it?',
+            abort=True
+        )
+
+    click.echo('Initializing the database...')
+    create_db()
+
+
+@cli.command()
+def add_data() -> None:
+    '''
+    Insert dummy data to the database file.
+    '''
+    create_db_if_not_exist()
+
+    topic = click.prompt('Topic', type=str)
+    phase = click.prompt('Phase (1-4)', type=int)
+    rating = click.prompt('Rating (1-5)', type=int)
+    detail = click.prompt('Detail', type=str)
+
+    connection = connect_db()
+    with connection:
+        connection.execute(
+            f'''
+            INSERT INTO reports (time, topic, phase, rating, detail)
+            VALUES ({time.time()}, "{topic}", {phase}, {rating}, "{detail}");
+        '''
+        )
+
+    connection.close()
+
+
+@cli.command()
+@click.option(
+    '--size', default=5, show_default=True, help='Number of records to print.'
+)
+def print_db(size: int) -> None:
+    '''
+    Print the data records in the database.
+    '''
+    create_db_if_not_exist()
+
+    connection = connect_db()
+    with connection:
+        records = connection.execute('SELECT * FROM reports').fetchmany(
+            size=size
+        )
+
+        if len(records) == 0:
+            click.echo('The database is empty.')
+        else:
+            for record in records:
+                print(record)
+
+    connection.close()
 
 
 def init_commands() -> None:
     '''
     Initialize all click commands.
     '''
-    cli()
+    try:
+        cli(standalone_mode=False)
+    except click.Abort:
+        # We don't want to throw click abort as error.
+        pass
