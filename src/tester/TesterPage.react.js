37e66b4dc1dbf771b0f7bc6bc9b0a3923af19fa3
dOo0oOb <kikicoco777us@gmail.com>
2022-11-09 13:40:57 -0800

    Update TesterPage.react.js
    add @@@ to Jia for testing

--- src/tester/TesterPage.react.js
+++ src/tester/TesterPage.react.js
@@ -8,5 +8,5 @@ import * as React from 'react';
 type Props = $ReadOnly<{}>;
 
 export default function TesterPage(_props: Props): React.MixedElement {
-  return <p>(Placeholder) This is the Tester Page</p>;
+  return <p>(Placeholder) This is the Tester Page@@@</p>;
 }
