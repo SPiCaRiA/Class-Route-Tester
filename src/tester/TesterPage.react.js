5e77e8f6a5812502a2cff6113c3e5933887a570c
dOo0oOb <kikicoco777us@gmail.com>
2022-11-11 13:06:59 -0800

    Update TesterPage.react.js

--- src/tester/TesterPage.react.js
+++ src/tester/TesterPage.react.js
@@ -3,10 +3,31 @@
  * @author Jia Yu
  */
 
-import * as React from 'react';
-
-type Props = $ReadOnly<{}>;
-
-export default function TesterPage(_props: Props): React.MixedElement {
-  return <p>(Placeholder) This is the Tester Page！！！</p>;
-}
+ import TesterHeader from './TesterHeader.react';
+ import TesterStepper from './TesterStepper.react';
+ import TesterTranscriptionCard from './TesterTranscriptionCard.react';
+ import TesterTranslationCard from './TesterTranslationCard.react';
+ 
+ import * as React from 'react';
+ 
+ type Props = $ReadOnly<{}>;
+ 
+ export default function TesterPage(_props: Props): React.MixedElement {
+   return (
+     <p>
+       <div>
+         <TesterHeader></TesterHeader>
+       </div>
+       <div>
+         <TesterStepper></TesterStepper>
+       </div>
+       <div>
+         <TesterTranscriptionCard></TesterTranscriptionCard>
+       </div>
+       <div>
+         <TesterTranslationCard></TesterTranslationCard>
+       </div>
+     </p>
+   );
+ }
+ 
