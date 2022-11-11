593b61bf2e5be6233593c5cf35824a1db4f9625d
dOo0oOb <kikicoco777us@gmail.com>
2022-11-11 13:11:00 -0800

    Create TesterHeader.react.js

---
+++ src/tester/TesterHeader.react.js
@@ -0,0 +1,22 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+export default function TesterHeader() {
+  return (
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+            ClassRoute Tester
+          </Typography>
+          <Button color="inherit">UPLOAD VIDEO</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
