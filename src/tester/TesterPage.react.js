166d4301956104e91a517ae533aa9c753ab09341
dOo0oOb <kikicoco777us@gmail.com>
2022-11-11 13:02:44 -0800

    Update TesterPage.react.js

--- src/tester/TesterPage.react.js
+++ src/tester/TesterPage.react.js
@@ -8,5 +8,9 @@ import * as React from 'react';
 type Props = $ReadOnly<{}>;
 
 export default function TesterPage(_props: Props): React.MixedElement {
+<<<<<<< Updated upstream
   return <p>(Placeholder) This is the Tester Page@@@</p>;
+=======
+  return <p>(Placeholder) This is the Tester Page！！！</p>;
+>>>>>>> Stashed changes
 }
