9578fe9bcdd7f20e3102b4a2344f5a0a4710a45a
dOo0oOb <kikicoco777us@gmail.com>
2022-11-11 13:25:27 -0800

    Create TesterTranslationTextField.react.js

---
+++ src/tester/TesterTranslationTextField.react.js
@@ -0,0 +1,31 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import * as React from 'react';
+
+export default function TesterTranslationTextField() {
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': {m: 1, width: 750},
+      }}
+      noValidate
+      autoComplete="off">
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label="Output"
+          multiline
+          rows={10}
+          defaultValue="Default Value"
+        />
+      </div>
+    </Box>
+  );
+}
