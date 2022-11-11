8f87ac48d567ad7e7b1a7bd7116f4be1c291faf8
dtat2328 <dtat@scu.edu>
2022-11-10 18:39:27 -0800

    Changed code to use MUI; added filter-table card

--- src/analytics/AnalyticsPage.react.js
+++ src/analytics/AnalyticsPage.react.js
@@ -5,20 +5,56 @@
 
 import 'AnalyticsPage.react.css';
 
+import {Typography} from '@mui/material';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
 import * as React from 'react';
 
 type Props = $ReadOnly<{}>;
 
+//const [phase] = React.useState('');
+
 export default function AnalyticsPage(_props: Props): React.MixedElement {
   return (
-    <div className="page-heading">
-      <div className="page-formatter">
-        <div className="page-formatter-1">
-          <div className="header-container">
-            <span className="class-route-tester-header">ClassRoute Tester</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container>
+        <Box
+          sx={{
+            height: '58px',
+            width: '1152px',
+            mt: '24px',
+            px: '24px',
+          }}>
+          <Typography variant="h4" fontWeight="400">
+            ClassRoute Tester
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            height: '991px',
+            width: '1152px',
+            mt: '24px',
+            pt: '24px',
+            px: '24px',
+          }}>
+          <Card
+            sx={{
+              bgcolor: '#FFFFFF',
+              textAlign: 'center',
+              borderRadius: '2px',
+              mx: '24px',
+              px: '24px',
+            }}>
+            <Box>(Placeholder) This is where the filter will go.</Box>
+            <Divider>RESULTS</Divider>
+            <Box>(Placeholder) This is where the table will go.</Box>
+          </Card>
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
