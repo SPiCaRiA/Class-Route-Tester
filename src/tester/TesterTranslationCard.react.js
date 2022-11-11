4fdef35702d460bf04e624527295c3936b9e28c6
dOo0oOb <kikicoco777us@gmail.com>
2022-11-11 13:25:09 -0800

    Create TesterTranslationCard.react.js

---
+++ src/tester/TesterTranslationCard.react.js
@@ -0,0 +1,25 @@
+import TesterTranslationTextField from './TesterTranslationTextField.react';
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+
+export default function TesterTranslationCard() {
+  return (
+    <Card sx={{maxWidth: 800}}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Translation
+          <TesterTranslationTextField></TesterTranslationTextField>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Rerun</Button>
+        <Button size="small">Report</Button>
+      </CardActions>
+    </Card>
+  );
+}
