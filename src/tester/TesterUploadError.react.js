8c23b422d9e32fc03f9688b8f8bfb4a17b2c16be
dOo0oOb <kikicoco777us@gmail.com>
2022-11-21 18:46:44 -0800

    Create TesterUploadError.react.js

---
+++ src/tester/TesterUploadError.react.js
@@ -0,0 +1,40 @@
+import {Button, Collapse, IconButton} from '@mui/material';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Stack from '@mui/material/Stack';
+import {Box} from '@mui/system';
+import * as React from 'react';
+
+export default function TesterUploadError() {
+  const [open, setOpen] = React.useState(false);
+
+  function openError() {
+    setOpen(true);
+  }
+
+  return (
+    <Box>
+      <Collapse in={open}>
+        <Stack sx={{width: '50%'}} spacing={2}>
+          <Alert
+            severity="error"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}>
+                X
+              </IconButton>
+            }>
+            <AlertTitle>Error</AlertTitle>
+            Failed to upload a video.
+          </Alert>
+        </Stack>
+      </Collapse>
+      <Button onClick={openError}>Trigger Error Alert</Button>
+    </Box>
+  );
+}
