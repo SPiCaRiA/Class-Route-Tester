25e444f6790e25b5363ee0e55ec57ecc7f51b3b7
dOo0oOb <104869323+dOo0oOb@users.noreply.github.com>
2022-11-21 19:03:17 -0800

    Update TesterRunError.react.js

--- src/tester/TesterRunError.react.js
+++ src/tester/TesterRunError.react.js
@@ -5,7 +5,7 @@ import Stack from '@mui/material/Stack';
 import {Box} from '@mui/system';
 import * as React from 'react';
 
-export default function TesterRunError() {
+export default function TesterRunErrorAlert() {
   const [open, setOpen] = React.useState(false);
 
   function openError() {
