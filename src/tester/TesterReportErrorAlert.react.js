bcff3c6b4f4c28bee36c70615bfd3d10442e008e
dOo0oOb <104869323+dOo0oOb@users.noreply.github.com>
2022-11-21 19:09:43 -0800

    Update and rename TesterReportError.react.js to TesterReportErrorAlert.react.js

--- src/tester/TesterReportErrorAlert.react.js
+++ src/tester/TesterReportErrorAlert.react.js
@@ -5,7 +5,7 @@ import Stack from '@mui/material/Stack';
 import {Box} from '@mui/system';
 import * as React from 'react';
 
-export default function TesterReportError() {
+export default function TesterReportErrorAlert() {
   const [open, setOpen] = React.useState(false);
 
   function openError() {
