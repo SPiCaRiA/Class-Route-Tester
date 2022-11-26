a83723700666a56413de03e024c34dfae6846808
dOo0oOb <104869323+dOo0oOb@users.noreply.github.com>
2022-11-26 01:23:14 -0800

    Update TesterHeader.react.js

--- src/tester/TesterHeader.react.js
+++ src/tester/TesterHeader.react.js
@@ -1,22 +1,24 @@
-import AppBar from '@mui/material/AppBar';
+import TesterUploadDialog from './TesterUploadDialog.react';
+
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 
 export default function TesterHeader() {
   return (
-    <Box sx={{flexGrow: 1}}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-            ClassRoute Tester
-          </Typography>
-          <Button color="inherit">UPLOAD VIDEO</Button>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <Toolbar
+        sx={{
+          m: 1,
+          width: 1000,
+        }}>
+        <Typography variant="h4" component="div" sx={{flexGrow: 1}}>
+          ClassRoute Tester
+        </Typography>
+        {/* <Button color="inherit">UPLOAD VIDEO</Button> */}
+        <TesterUploadDialog color="red">UPLOAD VIDEO</TesterUploadDialog>
+      </Toolbar>
     </Box>
   );
 }
