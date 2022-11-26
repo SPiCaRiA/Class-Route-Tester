ecdb66b0dd1d836f2ca3f61fcd59bd27ed3dcdfa
dOo0oOb <104869323+dOo0oOb@users.noreply.github.com>
2022-11-26 01:25:36 -0800

    Update TesterPage.react.js

--- src/tester/TesterPage.react.js
+++ src/tester/TesterPage.react.js
@@ -3,37 +3,38 @@
  * @author Jia Yu
  */
 
- import TesterHeader from './TesterHeader.react';
- import TesterStepper from './TesterStepper.react';
- import TesterTranscriptionCard from './TesterTranscriptionCard.react';
- import TesterTranslationCard from './TesterTranslationCard.react';
- import TesterRunErrorAlert from './TesterRunErrorAlert.react';
- 
- import Grid from '@mui/material/Grid';
- import * as React from 'react';
- 
- type Props = $ReadOnly<{}>;
- 
- export default function TesterPage(_props: Props): React.MixedElement {
-   return (
-     <p>
-       <div>
-         <TesterRunErrorAlert />
-         <TesterHeader></TesterHeader>
-       </div>
-       <Grid
-         sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-         <div>
-           <TesterStepper></TesterStepper>
-         </div>
-         <Grid sx={{m: 1, position: 'relative'}}>
-           <div>
-             <TesterTranscriptionCard></TesterTranscriptionCard>
-             <TesterTranslationCard></TesterTranslationCard>
-           </div>
-         </Grid>
-       </Grid>
-     </p>
-   );
- }
- 
+import TesterHeader from './TesterHeader.react';
+import TesterRunErrorAlert from './TesterRunErrorAlert.react';
+import TesterStepper from './TesterStepper.react';
+import TesterTranscriptionCard from './TesterTranscriptionCard.react';
+import TesterTranslationCard from './TesterTranslationCard.react';
+
+import Grid from '@mui/material/Grid';
+import * as React from 'react';
+
+type Props = $ReadOnly<{}>;
+
+export default function TesterPage(_props: Props): React.MixedElement {
+  return (
+    <p>
+      <div>
+        <TesterRunErrorAlert />
+      </div>
+      <div>
+        <TesterHeader></TesterHeader>
+      </div>
+      <Grid
+        sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <div>
+          <TesterStepper></TesterStepper>
+        </div>
+        <Grid sx={{m: 1, position: 'relative'}}>
+          <div>
+            <TesterTranscriptionCard></TesterTranscriptionCard>
+            <TesterTranslationCard></TesterTranslationCard>
+          </div>
+        </Grid>
+      </Grid>
+    </p>
+  );
+}
