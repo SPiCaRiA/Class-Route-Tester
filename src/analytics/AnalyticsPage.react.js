3bb4d6813deb81a0ad22b444af281e1d54ecb49b
dtat2328 <91914689+dtat2328@users.noreply.github.com>
2022-11-29 18:55:02 -0800

    Add error popup (#18)
    * 1st stage of changes
    
    * Add error alert
    
    * Center error alert on webpage
    
    * Addressed comments

--- src/analytics/AnalyticsPage.react.js
+++ src/analytics/AnalyticsPage.react.js
@@ -3,21 +3,37 @@
  * @author Daniel Tat
  */
 
+import AnalyticsErrorAlert from 'AnalyticsErrorAlert.react';
 import AnalyticsHeader from 'AnalyticsHeader.react';
-import MainCard from 'AnalyticsMainCard.react';
+import AnalyticsMainCard from 'AnalyticsMainCard.react';
 
+import {Collapse} from '@mui/material';
 import Container from '@mui/material/Container';
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
+import {useCallback, useState} from 'react';
 import * as React from 'react';
 
 type Props = $ReadOnly<{}>;
 
 export default function AnalyticsPage(_props: Props): React.MixedElement {
+  const [open, setOpen] = useState(false);
+
   return (
     <ScopedCssBaseline>
+      <Collapse in={open}>
+        <AnalyticsErrorAlert
+          closeButtonOnClick={useCallback(() => {
+            setOpen(false);
+          })}
+        />
+      </Collapse>
       <Container>
         <AnalyticsHeader />
-        <MainCard />
+        <AnalyticsMainCard
+          searchButtonOnClick={useCallback(() => {
+            setOpen(true);
+          })}
+        />
       </Container>
     </ScopedCssBaseline>
   );
